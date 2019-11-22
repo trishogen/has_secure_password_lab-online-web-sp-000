838f66c0ad7ad840c2988ac425f101046123e030
Trish Ogen <patriciaogen@gmail.com>
2019-11-22 00:59:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,8 @@ class SessionsController < ApplicationController
   end
 
   def create
+    raise params.inspect
+
     @user = User.find_by(name: params[:name])
     raise @user.inspect
     #if @user
