ea37c6d9dc677ce11c092ec47c92d22e43d9f375
Trish Ogen <patriciaogen@gmail.com>
2019-11-22 00:09:06 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,12 @@
 class UsersController < ApplicationController
-  
+
+  def create
+    User.create(user_params)
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :password, :password_confirmation)
+  end
 end
