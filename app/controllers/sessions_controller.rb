44e1cdd4072be4e5b31aadab76a41cc0dfeab82a
Trish Ogen <patriciaogen@gmail.com>
2019-11-22 00:42:17 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,12 @@ class SessionsController < ApplicationController
 
   def create
     @user = User.find_by(name: params[:name])
-    if @user
-      return head(:forbidden) unless @user.authenticate(params[:password])
+    #if @user
+    #  return head(:forbidden) unless @user.authenticate(params[:password])
       session[:user_id] = @user.id
-    else
-      redirect_to login_path
-    end
+    #else
+    #  redirect_to login_path
+    #end
   end
 
 
